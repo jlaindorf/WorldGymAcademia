a66a430da0f8003cab8f6d4a0fd300c962a0606e
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-04 16:00:53 -0300

    feat: Recupera o nome no localStorage

--- FitManageTech/src/views/Dashboard/Dashboard.vue
+++ FitManageTech/src/views/Dashboard/Dashboard.vue
@@ -1,3 +1,37 @@
 <template>
-     <h1>dashboard</h1>
+     <h1>Bem Vindo,{{ nome }}</h1>
+
+     <main>
+  <v-card
+    class="mx-auto"
+    width="400"
+    prepend-icon="mdi-account"
+  >
+    <template v-slot:title>
+      This is a title
+    </template>
+
+    <v-card-text>
+      This is content
+    </v-card-text>
+  </v-card>
+</main>
 </template>
+
+
+<script>
+export default {
+  data() {
+    const dadosUsuario = localStorage.getItem('user-info');
+    const valorChave = JSON.parse(dadosUsuario); 
+
+    return {
+      dadosUsuario: dadosUsuario,
+      valorChave: valorChave,
+      nome: valorChave.name, 
+    };
+  },
+};
+</script>
+
+
