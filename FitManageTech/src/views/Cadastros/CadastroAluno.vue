c058ec647fb01c1e0ac153a130d6584a8231a025
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-08 10:55:46 -0300

    Feat : Adiciona requisição Via CEP

--- FitManageTech/src/views/Cadastros/CadastroAluno.vue
+++ FitManageTech/src/views/Cadastros/CadastroAluno.vue
@@ -1,5 +1,5 @@
 <template>
-   <v-form>
+   <v-form ref="form" @click="cadastraAluno">
       <div class="logo">Fit Manage Tech
         <span class="mdi mdi-weight-lifter"></span>
        <h4 class="mdi mdi-account-multiple">Cadastro de Aluno</h4>
@@ -14,7 +14,9 @@
            sm="6"
          >
          <v-text-field
+         :rules="[value => !!value || 'Nome é obrigatório']"
              label="Nome Completo"
+             v-model="nome"
              variant="outlined"
            ></v-text-field>
          </v-col>
@@ -26,6 +28,8 @@
          <v-text-field
              label="Email"
              variant="outlined"
+             :rules="emailRules" 
+             v-model="email"
            ></v-text-field>
          </v-col>
  
@@ -35,6 +39,9 @@
          >
          <v-text-field
              label="Contato"
+             type="phone"
+             v-model="contato"
+             :rules="[value => !!value || 'Número de contato obrigatório']" 
              variant="outlined"
            ></v-text-field>
          </v-col>
@@ -49,7 +56,7 @@
       :max="new Date()"
       locale="pt-BR"
       :enable-time-picker="false"
-      v-model="selectedDate"
+      v-model="dataSelecionada"
       type="date"
     />
   
@@ -62,36 +69,57 @@
          >
          <v-text-field
              label="CEP"
+             @change="viacep"
+             v-model="cep"
              variant="outlined"
+             :rules="[value => !!value || 'CEP obrigatório']"
            ></v-text-field>
          </v-col>
  
          <v-col
            cols="12"
-           sm="6"
+           sm="4"
          >
          <v-text-field
              label="Logradouro"
+             v-model="logradouro"
              variant="outlined"
+             :rules="[value => !!value || 'Logradouro obrigatório']"
            ></v-text-field>
          </v-col>
+         
+         <v-col
+           cols="12"
+           sm="2"
+         >
+         <v-text-field
+             label="Número"
+             v-model="numero"
+             variant="outlined"
+             :rules="[value => !!value || 'Número obrigatório']"
+           ></v-text-field>
+         </v-col>
+
 
         <v-col
         cols="2"
         >
-        <v-select
-      label="Estado"
-      v-model="selectedState"
-      :items="estados"
-      outlined
-    ></v-select>
+        <v-text-field
+             label="Estado"
+             v-model="uf"
+             variant="outlined"
+             :rules="[value => !!value || 'Estado é obrigatório']"
+           ></v-text-field>
+    
         </v-col> 
         <v-col
         cols="3"
         >
         <v-text-field
              label="Cidade"
+             v-model="cidade"
              variant="outlined"
+             :rules="[value => !!value || 'Cidade é obrigatório']"
            ></v-text-field>
          </v-col>
          <v-col
@@ -99,7 +127,9 @@
        >
         <v-text-field
              label="Bairro"
+             v-model="bairro"
              variant="outlined"
+             :rules="[value => !!value || 'Bairro é obrigatório']"
            ></v-text-field>
          </v-col>
          <v-col
@@ -107,50 +137,55 @@
         >
         <v-text-field
              label="Complemento"
+             v-model="complemento"
              variant="outlined"
            ></v-text-field>
          </v-col>
        </v-row>
      </v-container>
+     <v-btn type="submit" color="blue" variant="tonal">Cadastrar</v-btn>
    </v-form>
+   
  </template>
  
 <script>
+import axios  from 'axios';
 export default {
   data() {
     return {
-      selectedDate: null,
-      selectedState: null,
-      estados: [
-        { text: 'Acre', value: 'AC' },
-        { text: 'Alagoas', value: 'AL' },
-        { text: 'Amapá', value: 'AP' },
-        { text: 'Amazonas', value: 'AM' },
-        { text: 'Bahia', value: 'BA' },
-        { text: 'Ceará', value: 'CE' },
-        { text: 'Distrito Federal', value: 'DF' },
-        { text: 'Espírito Santo', value: 'ES' },
-        { text: 'Goiás', value: 'GO' },
-        { text: 'Maranhão', value: 'MA' },
-        { text: 'Mato Grosso', value: 'MT' },
-        { text: 'Mato Grosso do Sul', value: 'MS' },
-        { text: 'Minas Gerais', value: 'MG' },
-        { text: 'Pará', value: 'PA' },
-        { text: 'Paraíba', value: 'PB' },
-        { text: 'Paraná', value: 'PR' },
-        { text: 'Pernambuco', value: 'PE' },
-        { text: 'Piauí', value: 'PI' },
-        { text: 'Rio de Janeiro', value: 'RJ' },
-        { text: 'Rio Grande do Norte', value: 'RN' },
-        { text: 'Rio Grande do Sul', value: 'RS' },
-        { text: 'Rondônia', value: 'RO' },
-        { text: 'Roraima', value: 'RR' },
-        { text: 'Santa Catarina', value: 'SC' },
-        { text: 'São Paulo', value: 'SP' },
-        { text: 'Sergipe', value: 'SE' },
-        { text: 'Tocantins', value: 'TO' },
-      ],
-    };
+      nome:'',
+      contato:'' , 
+      cep: '', 
+      logradouro: '',
+      complemento: '',
+      bairro: '',
+      cidade: '',
+      uf: '',
+      numero:'',
+      email: '',
+      emailRules: [ 
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail tem que ser válido'],
+      dataSelecionada: null,
+     
+    }
+  },
+  methods: {
+    viacep() {
+      axios({
+        url: `https://viacep.com.br/ws/${this.cep}/json/`,
+        method: 'GET',
+      })
+        .then((response) => {
+          this.data = response.data;
+          this.logradouro = this.data.logradouro;
+          this.bairro = this.data.bairro;
+          this.cidade = this.data.localidade;
+          this.uf = this.data.uf;
+        })
+        .catch(() => {
+          alert('Não foi possível Encontrar o endereço');
+        });
+    },
   },
 };
 </script>
