7e3f8aae01db47a570ab3f965de0d6d5a1ea4183
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-08 15:01:30 -0300

    Feat: Adiciona Validação do Botão

--- FitManageTech/src/views/Cadastros/CadastroAluno.vue
+++ FitManageTech/src/views/Cadastros/CadastroAluno.vue
@@ -1,223 +1,186 @@
 <template>
-   <v-form ref="form" @submit="cadastraAluno" >
+   <v-form ref="form" @submit="cadastraAluno">
       <div class="logo">Fit Manage Tech
-        <span class="mdi mdi-weight-lifter"></span>
-       <h4 class="mdi mdi-account-multiple">Cadastro de Aluno</h4>
-       
-        
+         <span class="mdi mdi-weight-lifter"></span>
+         <h4 class="mdi mdi-account-multiple">Cadastro de Aluno</h4>
+
+
       </div>
-     <v-container>
-       <v-row>
-         
-         <v-col
-           cols="12"
-           sm="6"
-         >
-         <v-text-field
-         :rules="[value => !!value || 'Nome é obrigatório']"
-             label="Nome Completo"
-             v-model="nome"
-             variant="outlined"
-           ></v-text-field>
-         </v-col>
- 
-         <v-col
-           cols="12"
-           sm="6"
-         >
-         <v-text-field
-             label="Email"
-             variant="outlined"
-             :rules="emailRules" 
-             v-model="email"
-           ></v-text-field>
-         </v-col>
- 
-         <v-col
-           cols="12"
-           sm="6"
-         >
-         <v-text-field
-             label="Contato"
-             type="phone"
-             v-model="contato"
-             :rules="[value => !!value || 'Número de contato obrigatório']" 
-             variant="outlined"
-           ></v-text-field>
-         </v-col>
- 
-         <v-col
-           cols="12"
-           sm="6"
-         >
-         <div>
-    <VueDatePicker
-      placeholder="Data De Nascimento"
-      :max="new Date()"
-      locale="pt-BR"
-      :enable-time-picker="false"
-      v-model="dataSelecionada"
-      type="date"
-    />
-  
-  </div>
-         </v-col>
- 
-         <v-col
-           cols="12"
-           sm="6"
-         >
-         <v-text-field
-             label="CEP"
-             @change="viacep"
-             v-model="cep"
-             variant="outlined"
-             :rules="[value => !!value || 'CEP obrigatório']"
-           ></v-text-field>
-         </v-col>
- 
-         <v-col
-           cols="12"
-           sm="4"
-         >
-         <v-text-field
-             label="Logradouro"
-             v-model="logradouro"
-             variant="outlined"
-             :rules="[value => !!value || 'Logradouro obrigatório']"
-           ></v-text-field>
-         </v-col>
-         
-         <v-col
-           cols="12"
-           sm="2"
-         >
-         <v-text-field
-             label="Número"
-             v-model="numero"
-             variant="outlined"
-             :rules="[value => !!value || 'Número obrigatório']"
-           ></v-text-field>
-         </v-col>
+      <v-container>
+         <v-row>
+
+            <v-col cols="12" sm="6">
+               <v-text-field :rules="[value => !!value || 'Nome é obrigatório']" label="Nome Completo" ref="nomeField"
+                  v-model="nome" variant="outlined"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6">
+               <v-text-field label="Email" variant="outlined" :rules="emailRules" v-model="email"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6">
+               <v-text-field label="Contato" type="phone" v-model="contato"
+                  :rules="[value => !!value || 'Número de contato obrigatório']" variant="outlined"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6">
+               <div>
+                  <VueDatePicker placeholder="Data De Nascimento" :max="new Date()" locale="pt-BR"
+                     :enable-time-picker="false" v-model="data" type="date" />
+
+               </div>
+            </v-col>
+
+            <v-col cols="12" sm="6">
+               <v-text-field label="CEP" @change="viacep" v-model="cep" variant="outlined"
+                  :rules="[value => !!value || 'CEP obrigatório']"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="4">
+               <v-text-field label="Logradouro" v-model="logradouro" variant="outlined"
+                  :rules="[value => !!value || 'Logradouro obrigatório']"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="2">
+               <v-text-field label="Número" v-model="numero" variant="outlined"
+                  :rules="[value => !!value || 'Número obrigatório']"></v-text-field>
+            </v-col>
 
 
-        <v-col
-        cols="2"
-        >
-        <v-text-field
-             label="Estado"
-             v-model="uf"
-             variant="outlined"
-             :rules="[value => !!value || 'Estado é obrigatório']"
-           ></v-text-field>
-    
-        </v-col> 
-        <v-col
-        cols="3"
-        >
-        <v-text-field
-             label="Cidade"
-             v-model="cidade"
-             variant="outlined"
-             :rules="[value => !!value || 'Cidade é obrigatório']"
-           ></v-text-field>
-         </v-col>
-         <v-col
-        cols="3"
-       >
-        <v-text-field
-             label="Bairro"
-             v-model="bairro"
-             variant="outlined"
-             :rules="[value => !!value || 'Bairro é obrigatório']"
-           ></v-text-field>
-         </v-col>
-         <v-col
-        cols="4"
-        >
-        <v-text-field
-             label="Complemento"
-             v-model="complemento"
-             variant="outlined"
-           ></v-text-field>
-         </v-col>
-       </v-row>
-     </v-container>
-     <v-btn type="submit" color="blue" variant="tonal">Cadastrar</v-btn>
+            <v-col cols="2">
+               <v-text-field label="Estado" v-model="uf" variant="outlined"
+                  :rules="[value => !!value || 'Estado é obrigatório']"></v-text-field>
+
+            </v-col>
+            <v-col cols="3">
+               <v-text-field label="Cidade" v-model="cidade" variant="outlined"
+                  :rules="[value => !!value || 'Cidade é obrigatório']"></v-text-field>
+            </v-col>
+            <v-col cols="3">
+               <v-text-field label="Bairro" v-model="bairro" variant="outlined"
+                  :rules="[value => !!value || 'Bairro é obrigatório']"></v-text-field>
+            </v-col>
+            <v-col cols="4">
+               <v-text-field label="Complemento" v-model="complemento" variant="outlined"></v-text-field>
+            </v-col>
+         </v-row>
+      </v-container>
+      <v-btn :disabled="!formularioValido" type="submit" color="blue" variant="tonal">Cadastrar</v-btn>
    </v-form>
-   
- </template>
+</template>
  
 <script>
-import axios  from 'axios';
+import axios from 'axios';
 export default {
-  data() {
-    return {
-      nome:'',
-      contato:'' , 
-      cep: '', 
-      logradouro: '',
-      complemento: '',
-      bairro: '',
-      cidade: '',
-      uf: '',
-      numero:'',
-      email: '',
-      emailRules: [ 
-        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail tem que ser válido'],
-      data: 'null',
-     
-    }
-  },
-  methods: {
-    viacep() {
-      axios({
-        url: `https://viacep.com.br/ws/${this.cep}/json/`,
-        method: 'GET',
-      })
-        .then((response) => {
-          this.data = response.data;
-          this.logradouro = this.data.logradouro;
-          this.bairro = this.data.bairro;
-          this.cidade = this.data.localidade;
-          this.uf = this.data.uf;
+   data() {
+      return {
+         nome: '',
+         contato: '',
+         cep: '',
+         logradouro: '',
+         complemento: '',
+         bairro: '',
+         cidade: '',
+         uf: '',
+         numero: '',
+         email: '',
+         emailRules: [
+            v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail tem que ser válido'],
+         data: 'null',
+         formularioValido: false
 
-         
-        })
-        .catch(() => {
-          alert('Não foi possível Encontrar o endereço');
-        });
-    },
-    cadastraAluno(){
-   axios({
-      url:'http://localhost:3000/students',
-      method: 'POST',
-      data: {
-            name: this.nome,
-            email: this.email,
-            contact: this.contato,
-            date_birth: this.data,
-            cep: this.cep,
-            street: this.logradouro,
-            number: this.numero,
-            neighborhood : this.bairro,
-            city: this.cidade,
-            province: this.uf,
-            complement: this.complemento
-         
+      }
+   },
+   mounted() {
+      this.$refs.nomeField.focus();
+   },
+   watch: {
 
-          }
-   })
-   .then(( response) =>{
+      nome: 'validarFormulario',
+      contato: 'validarFormulario',
+      cep: 'validarFormulario',
+      numero: 'validarFormulario'
 
-      alert('Aluno cadastrado com sucesso!')
-    
-   })
-   .catch(( ) => {
-           alert('Não foi possível cadastrar o ALuno ')
-          })
-  }
-  },
- 
+   },
+
+   methods: {
+      validarFormulario() {
+
+         this.formularioValido =
+            !!this.nome && this.contato && this.cep && this.numero
+            &&
+
+            true;
+      },
+      viacep() {
+         axios({
+            url: `https://viacep.com.br/ws/${this.cep}/json/`,
+            method: 'GET',
+         })
+            .then((response) => {
+               this.data = response.data;
+               this.logradouro = this.data.logradouro;
+               this.bairro = this.data.bairro;
+               this.cidade = this.data.localidade;
+               this.uf = this.data.uf;
+
+
+            })
+            .catch(() => {
+               alert('Não foi possível Encontrar o endereço');
+            });
+      },
+      cadastraAluno() {
+         axios({
+            url: 'http://localhost:3000/students',
+            method: 'POST',
+            data: {
+               name: this.nome,
+               email: this.email,
+               contact: this.contato,
+               date_birth: this.data,
+               cep: this.cep,
+               street: this.logradouro,
+               number: this.numero,
+               neighborhood: this.bairro,
+               city: this.cidade,
+               province: this.uf,
+               complement: this.complemento
+
+
+            }
+         })
+            .then((response) => {
+
+               alert('Aluno cadastrado com sucesso!');
+
+
+
+            })
+            .catch(() => {
+               alert('Não foi possível cadastrar o ALuno ')
+            })
+      }
+   },
+
 };
 </script>
+
+<style>
+.logo {
+   font-family: Arial, sans-serif;
+   font-size: 20px;
+   font-weight: bold;
+   color: #3498db;
+   /* Cor azul */
+   text-transform: uppercase;
+   letter-spacing: 4px;
+   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+   width: 80%;
+
+}
+
+.mdi {
+   width: 30%;
+}
+</style>
