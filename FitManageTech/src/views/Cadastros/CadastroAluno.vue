7efe54ef327c870a64fcb2596b078e53d26526ad
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-07 23:20:52 -0300

    Feat: Date picker e select Estados

--- FitManageTech/src/views/Cadastros/CadastroAluno.vue
+++ FitManageTech/src/views/Cadastros/CadastroAluno.vue
@@ -15,7 +15,6 @@
          >
          <v-text-field
              label="Nome Completo"
-             model-value="Insira seu nome"
              variant="outlined"
            ></v-text-field>
          </v-col>
@@ -26,7 +25,6 @@
          >
          <v-text-field
              label="Email"
-             model-value="Insira seu email"
              variant="outlined"
            ></v-text-field>
          </v-col>
@@ -37,7 +35,6 @@
          >
          <v-text-field
              label="Contato"
-             model-value="Insira seu telefone"
              variant="outlined"
            ></v-text-field>
          </v-col>
@@ -46,12 +43,17 @@
            cols="12"
            sm="6"
          >
-         <v-date-picker
-         
-          label="Data de Nascimento"
-          prepend-icon="mdi-calendar"
-          :rules="[value => !!value || 'A data de nascimento é obrigatória']"
-        ></v-date-picker>
+         <div>
+    <VueDatePicker
+      placeholder="Data De Nascimento"
+      :max="new Date()"
+      locale="pt-BR"
+      :enable-time-picker="false"
+      v-model="selectedDate"
+      type="date"
+    />
+  
+  </div>
          </v-col>
  
          <v-col
@@ -59,8 +61,7 @@
            sm="6"
          >
          <v-text-field
-             label="Nome Completo"
-             model-value="Insira seu nome"
+             label="CEP"
              variant="outlined"
            ></v-text-field>
          </v-col>
@@ -70,8 +71,42 @@
            sm="6"
          >
          <v-text-field
-             label="Nome Completo"
-             model-value="Insira seu nome"
+             label="Logradouro"
+             variant="outlined"
+           ></v-text-field>
+         </v-col>
+
+        <v-col
+        cols="2"
+        >
+        <v-select
+      label="Estado"
+      v-model="selectedState"
+      :items="estados"
+      outlined
+    ></v-select>
+        </v-col> 
+        <v-col
+        cols="3"
+        >
+        <v-text-field
+             label="Cidade"
+             variant="outlined"
+           ></v-text-field>
+         </v-col>
+         <v-col
+        cols="3"
+       >
+        <v-text-field
+             label="Bairro"
+             variant="outlined"
+           ></v-text-field>
+         </v-col>
+         <v-col
+        cols="4"
+        >
+        <v-text-field
+             label="Complemento"
              variant="outlined"
            ></v-text-field>
          </v-col>
@@ -79,3 +114,43 @@
      </v-container>
    </v-form>
  </template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      selectedDate: null,
+      selectedState: null,
+      estados: [
+        { text: 'Acre', value: 'AC' },
+        { text: 'Alagoas', value: 'AL' },
+        { text: 'Amapá', value: 'AP' },
+        { text: 'Amazonas', value: 'AM' },
+        { text: 'Bahia', value: 'BA' },
+        { text: 'Ceará', value: 'CE' },
+        { text: 'Distrito Federal', value: 'DF' },
+        { text: 'Espírito Santo', value: 'ES' },
+        { text: 'Goiás', value: 'GO' },
+        { text: 'Maranhão', value: 'MA' },
+        { text: 'Mato Grosso', value: 'MT' },
+        { text: 'Mato Grosso do Sul', value: 'MS' },
+        { text: 'Minas Gerais', value: 'MG' },
+        { text: 'Pará', value: 'PA' },
+        { text: 'Paraíba', value: 'PB' },
+        { text: 'Paraná', value: 'PR' },
+        { text: 'Pernambuco', value: 'PE' },
+        { text: 'Piauí', value: 'PI' },
+        { text: 'Rio de Janeiro', value: 'RJ' },
+        { text: 'Rio Grande do Norte', value: 'RN' },
+        { text: 'Rio Grande do Sul', value: 'RS' },
+        { text: 'Rondônia', value: 'RO' },
+        { text: 'Roraima', value: 'RR' },
+        { text: 'Santa Catarina', value: 'SC' },
+        { text: 'São Paulo', value: 'SP' },
+        { text: 'Sergipe', value: 'SE' },
+        { text: 'Tocantins', value: 'TO' },
+      ],
+    };
+  },
+};
+</script>
