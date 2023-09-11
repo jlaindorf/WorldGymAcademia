12592322719d798bb38b385da71c10f98797d631
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-11 20:56:32 -0300

    Feat: Inicia Formulário

--- FitManageTech/src/views/Cadastros/CadastroTreino.vue
+++ FitManageTech/src/views/Cadastros/CadastroTreino.vue
@@ -1,3 +1,109 @@
 <template>
-    <h1>cadastro de treino</h1>
-</template>
+    <v-form ref="form"  >
+       <div class="logo">Fit Manage Tech
+         <span class="mdi mdi-weight-lifter"></span>
+        <h4 class="mdi mdi-account-multiple">Cadastro de Treino</h4>
+        
+         
+       </div>
+      <v-container >
+        <v-row>
+            <v-col
+            cols="12"
+            sm="12"
+          >
+          <v-select  label="Qual Exercício" :items="['']" variant="outlined">
+          </v-select>
+          </v-col>
+         
+          <v-col
+            cols="12"
+            sm="4"
+          >
+          <v-text-field
+              label="Repetições"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+ 
+          <v-col
+            cols="12"
+            sm="4"
+          >
+          <v-text-field
+              label="Quilos"
+            
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+ 
+          <v-col
+            cols="12"
+            sm="4"
+          >
+          <v-text-field
+              label="Pausa em segundos"
+              :rules="[value => !!value || 'Dado obrigatório']" 
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="12"
+          >
+          <v-select  label="Dia da Semana" :items="['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']" variant="outlined">
+          </v-select>
+          </v-col>
+          <v-textarea
+             autocomplete="Observações"
+              label="Observações"
+            ></v-textarea>
+          <v-col
+            cols="12"
+            sm="12"
+          >
+        
+          </v-col>
+    
+          
+         
+        </v-row>
+      </v-container>
+      <v-btn color="blue" variant="tonal">Cadastrar</v-btn>
+    </v-form>
+ 
+  </template>
+ 
+ <script>
+ import axios  from 'axios';
+ export default {
+   data() {
+     return {
+       
+      
+     }
+   },
+     
+ 
+   methods: {
+   }}
+ </script>
+ 
+ <style>
+ .logo {
+    font-family: Arial, sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    color: #3498db;
+    /* Cor azul */
+    text-transform: uppercase;
+    letter-spacing: 4px;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+    width: 80%;
+ 
+ }
+ 
+ .mdi {
+    width: 30%;
+ }
+ </style>
