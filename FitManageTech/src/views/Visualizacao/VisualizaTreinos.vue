b79e57cf12b114d2b94adb19360699c8b3b86d21
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-15 15:54:30 -0300

    Feat: inicia tabela de treino por dia da semana

--- FitManageTech/src/views/Visualizacao/VisualizaTreinos.vue
+++ FitManageTech/src/views/Visualizacao/VisualizaTreinos.vue
@@ -1,58 +1,71 @@
 <template>
-   <h1> visualiza treinos</h1>
-</template>
-   <v-table
-     fixed-header
-     height="300px"
-   >
-     <thead>
-       <tr>
-         <th class="text-left">
-           Segunda
-         </th>
-       </tr>
-     </thead>
-     <tbody>
-      <tr v-for="workout in listaTreinos" :key="workout.id"> 
-        <td>{{ workout.exercise_id }}</td>
-        <td>{{ workout.height }}</td>
-        <td>{{ workout.repetitions }}</td>
-        <td>{{ workout.break_time }}</td>
-      </tr>
-     </tbody>
-   </v-table>
+   <div>
+     <h1>Visualizar Treinos</h1>
+     
+     <table>
+       <thead>
+      
+         <tr>
+           <th>Dia da Semana</th>
+           <th>Exercício</th>
+           <th>Repetições</th>
+           <th>Pausa (s)</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr v-for="workout in listaTreinos" :key="workout.id">
+           <td>{{ workout.day }}</td>
+           <td>{{ workout.exercise_description}}</td>
+           <td>{{ workout.repetitions }}</td>
+           <td>{{ workout.break_time }}</td>
+         </tr>
+       </tbody>
+     </table>
+   </div>
  </template>
+ 
  <script>
- import axios from 'axios'
-   export default {
-     data () {
-       return {
-         studentId:'this.$route.params.id' ,
-        listaTreinos: [],
-        workout:'',
-        exercise_id: ''
-        
-       }
-     }, mounted(){
-      this.loadWorkout()
-   
+ import axios from 'axios';
+ 
+ export default {
+   data() {
+     return {
+       studentId: this.$route.params.id,
+       listaTreinos: [],
+       seg: '',
+       terca: '',
+       qua: '',
+       qui: '',
+       sex: '',
+       sab: '',
+       dom: '',
+     };
    },
-     loadWorkout(){
-      axios({
-        url: `http://localhost:3000/workouts?student_id=${this.studentId}`,
-        method: 'GET',
-      })
-        .then((response) => {
-            this.listaTreinos = response.data
-            this.exercise_id = response.data.exercise_id
-            
-        })
-         
-        .catch(() => {
-          alert('Não foi possível Acessar a Lista de Exercícios')
-        })
-      },
-    
-
-     }
-   
+   mounted() {
+     this.loadWorkout();
+   },
+   methods: {
+     loadWorkout() {
+       axios({
+         url: `http://localhost:3000/workouts?student_id=${this.studentId}`,
+         method: 'GET',
+       })
+         .then((response) => {
+           this.listaTreinos = response.data.workouts
+       
+           this.seg = response.data.segunda
+           this.terca = response.data.terca
+           this.qua = response.data.quarta
+           this.qui = response.data.quinta
+           this.sex = response.data.sexta
+           this.sab = response.data.sabado
+           this.dom = response.data.domingo
+         })
+         .catch(() => {
+           alert('Não foi possível acessar a lista de exercícios.')
+         });
+     },
+   },
+ }
+ </script>
+ 
