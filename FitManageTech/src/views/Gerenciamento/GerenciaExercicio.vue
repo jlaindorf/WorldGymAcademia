b959144d670f5ec282f231c1f63e94f21e24dc87
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-16 16:32:10 -0300

    Fix: Corrige validação cadastro exercício

--- FitManageTech/src/views/Gerenciamento/GerenciaExercicio.vue
+++ FitManageTech/src/views/Gerenciamento/GerenciaExercicio.vue
@@ -1,25 +1,25 @@
 <template>
-    <div>
-        <div class="logo">Fit Manage Tech
-        <span class="mdi mdi-weight-lifter" ></span>
-      </div>
-      <h1>Gerenciamento de Exercícios</h1>
+  <div>
+    <div class="logo">Fit Manage Tech
+      <span class="mdi mdi-weight-lifter"></span>
     </div>
+    <h1>Gerenciamento de Exercícios</h1>
+  </div>
 
-    <v-form ref="form" @submit="getExercise">
-        <v-row align="center" justify="center">
-    <v-col cols="6"> 
-        <v-text-field label="Exercício" prepend-icon="mdi-arm-flex"
-         v-model="exercicio" :rules="[value => !!value || 'É obrigatório colocar o exercício']"  ></v-text-field>
-    </v-col>
-    <v-col cols="2"> 
-      <v-btn type="submit" color="blue" variant="tonal">Cadastrar</v-btn>
-    </v-col>
-  </v-row>
+  <v-form ref="form" @submit="getExercise">
+    <v-row align="center" justify="center">
+      <v-col cols="6">
+        <v-text-field label="Exercício" prepend-icon="mdi-arm-flex" v-model="exercicio"
+          :rules="[value => !!value || 'É obrigatório colocar o exercício']"></v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <v-btn type="submit" color="blue" variant="tonal">Cadastrar</v-btn>
+      </v-col>
+    </v-row>
 
-</v-form>
+  </v-form>
 
-<v-table>
+  <v-table>
     <thead>
       <tr>
         <th class="text-left">Exercícios cadastrados</th>
@@ -31,10 +31,6 @@
       </tr>
     </tbody>
   </v-table>
-
- 
-
-  
 </template>
 
 
@@ -47,7 +43,7 @@ export default {
     return {
       exercicio: '',
       listaExercicios: []
-     
+
     };
   },
   mounted() {
@@ -55,7 +51,7 @@ export default {
   },
 
   methods: {
-    
+
     loadExercises() {
 
       axios({
@@ -63,45 +59,57 @@ export default {
         method: 'GET',
       })
         .then((response) => {
-            this.listaExercicios = response.data
+          this.listaExercicios = response.data
         })
-         
+
         .catch(() => {
           alert('Não foi possível Acessar a Lista de Exercícios')
         })
     },
-   
-    
-   getExercise() {
-      
+
+
+    //  getExercise() {
+    async getExercise() {
+      const { valid } = await this.$refs.form.validate()
+
+      if (!valid) {
+        alert("Preencha todos os dados!")
+        return
+      }
+      else {
         axios({
-  url: 'http://localhost:3000/exercises',
-  method: 'POST',
-  data: {
-    description: this.exercicio
+          url: 'http://localhost:3000/exercises',
+          method: 'POST',
+          data: {
+            description: this.exercicio
+          }
+        }).then((response) => {
+          alert('Exercício cadastrado com sucesso!')
+        }).catch((error) => {
+          console.error(error);
+        });
+      }
+    }
   }
-}).then((response) => {
-  alert('Exercício cadastrado com sucesso!')
-}).catch((error) => {
-  console.error(error);
-});}
- 
-  }}
+}
 </script>
 
 
 <style>
-  .logo {
-            font-family: Arial, sans-serif;
-            font-size: 20px;
-            font-weight: bold;
-            color: #3498db; /* Cor azul */
-            text-transform: uppercase;
-            letter-spacing: 4px;
-            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-            width: 80%;
-           
-        }
-        .mdi {
-          width: 30%;}
+.logo {
+  font-family: Arial, sans-serif;
+  font-size: 20px;
+  font-weight: bold;
+  color: #3498db;
+  /* Cor azul */
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+  width: 80%;
+
+}
+
+.mdi {
+  width: 30%;
+}
 </style>
